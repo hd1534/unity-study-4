fbdcc38deb3bb5875113367e02c07bcb6d234a03
nahjh2 <nahjh2@gmail.com>
2021-03-29 23:48:17 +0900

    player move with animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,23 +8,34 @@ public class Player : MonoBehaviour
     Rigidbody2D rigid;
 
     Vector2 velocity;
+    Animator animator;
     bool isVertical;
     private void Awake() {
         rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     void Update()
     {
         // 대각선 움직임 제한
-        if (Input.GetButtonDown("Vertical") || Input.GetButtonUp("Horizontal"))
+        if (Input.GetButtonDown("Vertical") || Input.GetButtonUp("Horizontal")) {
             isVertical = true;
-        if (Input.GetButtonDown("Horizontal") || Input.GetButtonUp("Vertical"))
+            animator.SetTrigger("Change");
+        }
+        if (Input.GetButtonDown("Horizontal") || Input.GetButtonUp("Vertical")) {
             isVertical = false;
-            
+            animator.SetTrigger("Change");
+        }
 
-        if (isVertical) 
-            velocity = new Vector2(0, Input.GetAxisRaw("Vertical")* speed);
-        else 
+        if (isVertical) {
+            velocity = new Vector2(0, Input.GetAxisRaw("Vertical") * speed);
+            animator.SetInteger("vAxisRaw", (int)Input.GetAxisRaw("Vertical"));
+            animator.SetInteger("hAxisRaw", 0);
+        }
+        else { 
             velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, 0);
+            animator.SetInteger("vAxisRaw", 0);
+            animator.SetInteger("hAxisRaw", (int)Input.GetAxisRaw("Horizontal"));
+        }
 
         rigid.velocity = velocity;
     }
